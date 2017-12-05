259e84e43cc443ae7e57437299968ba3cc23ea60
Fredrik Ekre <fredrik.ekre@chalmers.se>
2017-12-05 23:19:28 +0100

    wip

---
+++ base/similar.jl
@@ -0,0 +1,8 @@
+# This file is a part of Julia. License is MIT: https://julialang.org/license
+
+# module Similar
+function similar(a::A, ::Type{T}, dims::DimsOrInd) where {A,T}
+
+end
+
+# end # module
